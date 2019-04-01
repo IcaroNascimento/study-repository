431ae47fb2719b385aaec6c6254c43e59bc29818
Icaro Nascimento <icaronascimento1@gmail.com>
2019-03-31 23:27:31 -0300

    lowercase.js: lowercase function with tests

---
+++ tolowercase/lowercase.js
@@ -0,0 +1,12 @@
+var letter ;
+
+function writelowercase(letter) {
+
+letter = 'A'
+
+return letter.toLowerCase();
+
+
+}
+
+module.exports = writelowercase;
---
+++ tolowercase/lowercase.test.js
@@ -0,0 +1,7 @@
+const writelowercase = require('./lowercase');
+
+test('should change a letter uppercase to lowercase',() =>{
+
+expect(writelowercase()).toEqual('a');
+
+});
